793d7e1d62b5af109ea3d48b06781f9f086a9c3f
AnnUC <xiaoanding@uchicago.edu>
2017-11-29 21:29:30 -0600

    new

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -1087,7 +1087,7 @@ mutex_t register_heap_info_lock = _LIBC_LOCK_INITIALIZER;
 int is_resgistered_heap_info = 0; 
 int* register_heap_info_flag;
 int flag_counter = 0;
-#define NUM_HEAP_INFO_FLAG 1024;
+#define NUM_HEAP_INFO_FLAG 1024
 void register_heap_info (int mem_allocator_identifier, void* arena_start_ptr,
                          void* subheap_start_ptr, size_t subheap_size,
                          int* new_error_info_flag);
@@ -2562,7 +2562,6 @@ sysmalloc (INTERNAL_SIZE_T nb, mstate av)
         {
           if (mp_.sbrk_base == 0) {
             mp_.sbrk_base = brk;
-            int* flag_loc = 0;
 
             (void) mutex_lock (&register_heap_info_lock);
             while (register_heap_info_flag[flag_counter] != -1)
